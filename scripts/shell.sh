beccb0ab4be388f9aa300e0fa9fab900388aea1f
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2015-11-30 23:10:13 +0200

    fix nix-shell-root

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,15 +4,15 @@ def='(s: {})'
 overrides="${1:-$def}"
 app=$(if [ -f "$PWD/shell.nix" ]; then echo "$PWD/shell.nix"; else echo "$PWD/default.nix"; fi;)
 nix-shell -p haskellPackages.cabal2nix --command "cabal2nix . > default.nix"
-foo=$(grep ghcjs $app)
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+foo=$(grep ghcjs $app)
 if [ $? -eq 0 ]; then
-  nix-shell $dir/shell.nix --arg app $app --arg overrides "$overrides" --command "PATH_SUFFIX=\$PATH PROMPT_PREFIX='%{\$fg_bold[red]%}' exec zsh; return" --argstr compiler ghcjs
+  nix-shell $dir/shell.nix --arg app $app --arg overrides "$overrides" --command "PATH_SUFFIX=\$PATH PROMPT_PREFIX='%{\$fg_bold[red]%}' NIX_SHELL_ROOT=\$PWD exec zsh; return" --argstr compiler ghcjs
 else
   bar=$(grep haste-compiler $app)
   if [ $? -eq 0 ]; then
-    nix-shell $dir/shell.nix --arg app $app --arg overrides "$overrides" --command "PATH_SUFFIX=\$PATH PROMPT_PREFIX='%{\$fg_bold[red]%}' exec zsh; return" --argstr compiler ghc784
-  else
-    nix-shell $dir/shell.nix --arg app $app --arg overrides "$overrides" --command "PATH_SUFFIX=\$PATH PROMPT_PREFIX='%{\$fg_bold[red]%}' exec zsh; return"
+    nix-shell $dir/shell.nix --arg app $app --arg overrides "$overrides" --command "PATH_SUFFIX=\$PATH PROMPT_PREFIX='%{\$fg_bold[red]%}' NIX_SHELL_ROOT=\$PWD exec zsh; return" --argstr compiler ghc784
+else
+    nix-shell $dir/shell.nix --arg app $app --arg overrides "$overrides" --command "PATH_SUFFIX=\$PATH PROMPT_PREFIX='%{\$fg_bold[red]%}'  NIX_SHELL_ROOT=\$PWD exec zsh; return"
   fi
 fi
